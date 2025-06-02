8a29906b5418082304b81dba73b9124df90e6d27
ulaurent <ulaurent12@gmail.com>
2025-06-01 20:26:38 -0400

    initial commit

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -10,12 +10,36 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapView()
+                .frame(height: 300)
+                .cornerRadius(8)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                    .foregroundColor(Color.black)
+                HStack {
+                    Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                
+                Divider()
+                
+                Text("A natural rock formation resembling a turtle, Turtle Rock is a popular spot for hikers and photographers.")
+                    .font(.caption)
+                    .foregroundColor(Color.gray)
+            }
         }
         .padding()
+        Spacer()
+
     }
 }
 
